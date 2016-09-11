a9b5004760033a09c4c28bf05c64d0f2cf2e57b2
Anthony Hawley <tony@tamedo.com>
2016-09-11 03:03:14 +0100

    tidy spaces

--- public/app/components/Jobs.js
+++ public/app/components/Jobs.js
@@ -28,26 +28,26 @@ var Jobs = React.createClass({
   render: function() {
     var hh = this.handleClick;
     return (
-			<div className="row">
-				<div className="col-xs-12">
-					<div className="page-header text-center">
-						<h1>Experience<br /><small>A selection of my recent work</small></h1>
-					</div>
-				</div>
-				<div className="col-xs-12">
-	        {this.state.jobs.map(function(job) {
-	          return (
-							<div className="item-job" onClick={hh}>
-	                <h3>{job.company}</h3>
-	                <ul>
-	                    <li>{job.summary}</li>
-	                </ul>
-	                <p><i><small>HTML, CSS, Javascript, Jquery, Wordpress, PHP</small></i></p>
-	            </div>
-	          );
-	        })}
-				</div>
+    <div className="row">
+      <div className="col-xs-12">
+        <div className="page-header text-center">
+          <h1>Experience<br /><small>A selection of my recent work</small></h1>
+        </div>
       </div>
+      <div className="col-xs-12">
+      {this.state.jobs.map(function(job) {
+        return (
+          <div className="item-job" onClick={hh}>
+            <h3>{job.company}</h3>
+            <ul>
+              <li>{job.summary}</li>
+            </ul>
+            <p><i><small>HTML, CSS, Javascript, Jquery, Wordpress, PHP</small></i></p>
+          </div>
+        );
+      })}
+      </div>
+    </div>
     )
   }
 });
